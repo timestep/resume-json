149cb43a88fb4d2268fdac782aa62664a169a893
Kevin Sivabalan <kevin.sivabalan@rangle.io>
2018-03-31 13:19:11 -0400

    Completed Parsing

--- app/main/jsonBuilder/index.js
+++ app/main/jsonBuilder/index.js
@@ -1,63 +1,35 @@
 import { createTextNode } from '../../html-modifiers';
+import { flatten } from 'ramda';
 
 export const jsonBuilder = json => {
-  return Object.keys(json).reduce(jsonReducer(json), []);
+  const x = Object.keys(json).reduce(jsonReducer(json), []);
+  return flatten(x);
 };
 
-const arrayJsonBuilder = arrayJson => {
-  return arrayJson.reduce(jsonReducer(arrayJson), []);
-};
-
-const jsonLoop = j => {
-  if (typeof j === 'string') {
-    return createTextNode('div', j);
-  }
-  if (Array.isArray(j)) {
-    return json.map(jsonLoop);
-  }
-
-  if (typeof j === 'object' && !Array.isArray(j)) {
-    return jsonBuilder(j);
-  }
+export const arrayJsonBuilder = arrayJson => {
+  const x = arrayJson.reduce(jsonReducer(arrayJson), []);
+  return flatten(x);
 };
 
 const jsonReducer = json => (acc, key) => {
-  debugger;
-  if (typeof json === 'string') {
-    return acc.concat(createTextNode('div', key));
-  }
-  if (Array.isArray(json)) {
-    debugger;
-    return acc.concat(json.map(jsonLoop));
-    // debugger;
-    // return (
-    //   acc
-    //     // .concat(createTextNode('h2', key))
-    //     .concat(arrayJsonBuilder(json))
-    // );
-  }
-
   switch (typeof json[key]) {
     case 'string':
     case 'number':
-      return acc
-        .concat(createTextNode('h3', key))
-        .concat(createTextNode('div', json[key]));
+      return acc.concat(createTextNode('div', json[key]));
 
     case 'object':
       if (Array.isArray(json[key])) {
-        debugger;
-        return acc
-          .concat(createTextNode('h2', key))
-          .concat(arrayJsonBuilder(json[key]));
+        return acc.concat(arrayJsonBuilder(json[key]));
       } else {
-        return acc
-          .concat(createTextNode('h2', key))
-          .concat(jsonBuilder(json[key]));
+        return acc.concat(jsonBuilder(json[key]));
       }
     case 'undefined':
       if (typeof key === 'string') {
         return acc.concat(createTextNode('div', key));
+      }
+      if (Array.isArray(json)) {
+        const x = json.map(x => jsonBuilder(x));
+        return acc.concat(x);
       }
     default:
       console.log('Sorry, we are out of Mangos');
--- app/main/main.js
+++ app/main/main.js
@@ -1,13 +1,19 @@
 import resume from '../../assets/resume.json';
-import { jsonBuilder } from './jsonBuilder';
+import { jsonBuilder, arrayJsonBuilder } from './jsonBuilder';
 
 export const main = () => {
-  console.log(resume);
-  // const profile = jsonBuilder('profile', resume.profile);
-  // const experience = jsonBuilder('experience', resume.experience);
-  const education = jsonBuilder(resume.education);
+  const profile = jsonBuilder(resume.profile);
+  const experience = jsonBuilder(resume.experience);
+  const education = resume.education.map(x => jsonBuilder(x));
 
-  // profile.forEach(x => document.body.appendChild(x));
-  education.forEach(x => document.body.appendChild(x));
-  // document.body.appendChild(experience);
+  profile.forEach(x => document.body.appendChild(x));
+  education.forEach(loopAppend);
+  experience.forEach(x => document.body.appendChild(x));
+};
+
+const loopAppend = x => {
+  if (Array.isArray(x)) {
+    return x.forEach(loopAppend);
+  }
+  return document.body.appendChild(x);
 };
