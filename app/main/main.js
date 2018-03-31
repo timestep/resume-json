decd15e54f4654e9e8139a84c962f43b622badd5
Kevin Sivabalan <kevin.sivabalan@rangle.io>
2018-03-31 10:19:32 -0400

    Adding Build Step Changing Folder Structure Resume Finalization

--- app/main/main.js
+++ app/main/main.js
@@ -1,12 +1,11 @@
 import resume from '../../assets/resume.json';
-import { createTextDiv } from '../../html-modifiers';
+import { createTextDiv } from '../html-modifiers';
 
 export const main = () => {
   console.log('hi');
 
   console.log(resume);
-  console.log(JSON.stringify(resume));
 
-  var title = createTextDiv('title');
+  var title = createTextDiv(JSON.stringify(resume));
   document.body.appendChild(title);
 };
