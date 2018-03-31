0d95462313360273c8145d67ab153de0a645eb10
Kevin Sivabalan <kevin.sivabalan@rangle.io>
2018-03-31 12:04:00 -0400

    Running into Max Stack Call Error

--- app/main/jsonBuilder/index.js
+++ app/main/jsonBuilder/index.js
@@ -1,40 +1,39 @@
 import { createTextNode } from '../../html-modifiers';
 
 export const jsonBuilder = (title, json) => {
-  const node = createTextNode('div', title);
-  Object.keys(json)
-    .reduce(jsonReducer(json), [])
-    .forEach(x => node.appendChild);
-  return node;
+  return Object.keys(json).reduce(jsonReducer(json), []);
 };
 
 const arrayJsonBuilder = (title, arrayJson) => {
-  const node = createTextNode('div', title);
-  arrayJson
-    .reduce(jsonReducer(arrayJson), [])
-    .forEach(x => node.appendChild(x));
-  return node;
+  return arrayJson.reduce(jsonReducer(arrayJson), []);
 };
 
 const jsonReducer = json => (acc, key) => {
   switch (typeof json[key]) {
     case 'string':
-      acc.push(createTextNode('h3', key));
-      acc.push(createTextNode('div', json[key]));
-      break;
+    case 'number':
+      return acc
+        .concat(createTextNode('h3', key))
+        .concat(createTextNode('div', json[key]));
+
     case 'object':
       if (Array.isArray(json[key])) {
-        acc.push(createTextNode('h2', key));
-        return arrayJsonBuilder(key, json[key]);
+        return acc
+          .concat(createTextNode('h2', key))
+          .concat(arrayJsonBuilder(key, json[key]));
       } else {
-        acc.push(createTextNode('h2', key));
-        return jsonBuilder(key, json[key]);
+        return acc
+          .concat(createTextNode('h2', key))
+          .concat(jsonBuilder(key, json[key]));
       }
     case 'undefined':
-      switch (typeof key) {
-        case 'string':
-          acc.push(createTextNode('div', key));
-          return acc;
+      if (typeof key === 'string' || typeof key === 'number') {
+        return acc.concat(createTextNode('div', key));
+      }
+      if (Array.isArray(json)) {
+        return acc
+          .concat(createTextNode('h2', key))
+          .concat(arrayJsonBuilder(key, json));
       }
       break;
     default:
--- app/main/main.js
+++ app/main/main.js
@@ -3,6 +3,11 @@ import { jsonBuilder } from './jsonBuilder';
 
 export const main = () => {
   console.log(resume);
-  const resumeEl = jsonBuilder('Resume', resume);
-  document.body.appendChild(resumeEl);
+  const profile = jsonBuilder('profile', resume.profile);
+  const education = jsonBuilder('education', resume.education);
+  const experience = jsonBuilder('experience', resume.experience);
+
+  document.body.appendChild(profile);
+  document.body.appendChild(education);
+  document.body.appendChild(experience);
 };
